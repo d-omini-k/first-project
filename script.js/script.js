41567261a4c699c9bf6fd134eb88d4efd6a724af
d-omini-k <invoker3378@gmail.com>
2022-10-28 14:02:59 +0300

    new function for random colors and stoped colors

--- script.js/script.js
+++ script.js/script.js
@@ -1,5 +1,29 @@
 const cols = document.querySelectorAll('.color')
 
+document.addEventListener('keydown',(event) =>{
+  event.preventDefault()
+  if (event.code.toLowerCase() == 'space'){
+    setRandomColors()
+  }
+})
+
+document.addEventListener('click', event => {
+  const type = event.target.dataset.type
+
+  if (type == 'lock'){
+
+    const node = event.target.tagName.toLowerCase() == 'i'
+    ? event.target
+    : event.target.children[0]
+
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
+  } else if (type == 'copy'){
+    copyToClickboard(event.target.textContent)
+  }
+})
+
+
 function generateRandomColor(){
 
   const hexCodes = '0123456789ABCDEF'
@@ -10,14 +34,51 @@ function generateRandomColor(){
   return '#' + color
 }
 
-function setRandomColors(){
 
-cols.forEach(col =>{
+function copyToClickboard(text){
+ return navigator.clipboard.writeText(text)
+
+}
+
+function setRandomColors(){
+ const colors = []
+
+cols.forEach((col) =>{
+const isLocked = col.querySelector('i').classList.contains('fa-lock')
 const text = col.querySelector('h2')
-const color = generateRandomColor() 
+const button = col.querySelector('button')
+const color = chroma.random() 
+
+if(isLocked){
+  colors.push(text.textContent)
+  return 
+}
+
+colors.push(color)
 
 text.textContent = color
 col.style.background = color
+
+setTextColor(text, color)
+setTextColor(button, color)
   })
+
+updateColorsHash(colors)
 }
+
+function setTextColor (text,color){
+
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.5 ? 'black':'white'
+}
+
+
+function updateColorsHash(colors = []){
+document.location.hash = colors
+  .map((col) => {
+  return col.toString().substring(1)
+  })
+  .join('-')
+}
+
 setRandomColors()
